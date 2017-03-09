0c3487e8e9a560202a4235ae36d4f34a405b33b6
Dash Peters <dash098@gmail.com>
2017-03-09 03:03:24 -0800

    Fix typos

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 
-setup(name='pydictionay2',
+setup(name='pydictionary2',
       version='1.0',
       description='A python module to get meanings, synonyms and antonyms',
       url='http://github.com/LordFlashmeow/PyDictionary2',
       author='LordFlashmeow',
       author_email='dash098@gmail.com',
       license='MIT',
-      packages=['pydictionay2'],
+      packages=['pydictionary2'],
       install_requires=[
           'requests',
           'BeautifulSoup4'
-      ]
-      zip_safe=False)
+      ])
